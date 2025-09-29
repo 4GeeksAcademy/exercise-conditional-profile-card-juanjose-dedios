626028b718d206cd2182a129f596a8f8eb6ba32a
juanjooddm <juanjoseddm17052009@gmail.com>
2025-09-29 19:30:08 +0000

    finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -45,22 +45,22 @@ function render(variables = {}) {
               variables.twitter
                 ? `https://twitter.com/${variables.twitter}`
                 : "https://twitter.com/4geeksacademy"
-            } <i class="fab fa-twitter"></i></a></li>
-            <li><a href ${
+            }> <i class="fab fa-twitter"></i></a></li>
+            <li><a href= ${
               variables.github
-                ? variables.github
+                ? `https://twitter.com/${variables.github}`
                 : " https://github.com/4geeksacademy"
-            }><i class="fab fa-github"></i></a></li>
-            <li><a href ${
+            }> <i class="fab fa-github"></i></a></li>
+            <li><a href= ${
               variables.linkedin
-                ? variables.linkedin
+                ? `https://twitter.com/${variables.linkedin}`
                 : "https://linkedin.com/school/4geeksacademy"
-            }<i class="fab fa-linkedin"></i></a></li>
-            <li><a href ${
+            }> <i class="fab fa-linkedin"></i></a></li>
+            <li><a href= ${
               variables.instagram
-                ? variables.instagram
+                ? `https://twitter.com/${variables.instagram}`
                 : "https://instagram.com/4geeksacademy"
-            }<i class="fab fa-instagram"></i></a></li>
+            }> <i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
